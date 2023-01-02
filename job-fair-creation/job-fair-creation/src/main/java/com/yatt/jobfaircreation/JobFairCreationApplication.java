2b429ccf83a00fd6f282b6d435a097e8f83f36c6
theodros <ayichiluhm.theodros@gmail.com>
2023-01-02 13:18:10 -0600

    added job fair and discovery client into aws

---
+++ job-fair-creation/job-fair-creation/src/main/java/com/yatt/jobfaircreation/JobFairCreationApplication.java
@@ -0,0 +1,13 @@
+package com.yatt.jobfaircreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobFairCreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobFairCreationApplication.class, args);
+	}
+
+}
---
+++ job-fair-creation/job-fair-creation/src/test/java/com/yatt/jobfaircreation/JobFairCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yatt.jobfaircreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobFairCreationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
